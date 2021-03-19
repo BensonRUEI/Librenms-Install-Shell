20fda83d65ace5b0b599f75740a25df350417ea3
Benson <k4232@gcloud.csu.edu.tw>
2021-03-20 00:06:16 +0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -86,6 +86,7 @@ systemctl restart php7.4-fpm
 
 #設定PHP-FPM
 #cp /etc/php/7.4/fpm/pool.d/www.conf /etc/php/7.4/fpm/pool.d/librenms.conf
+sed -i 's/\[www\]/\[librenms\]/g' /etc/php/7.4/fpm/pool.d/librenms.conf
 
 #設定NGINX
 echo		server {	 >> /etc/nginx/conf.d/librenms.conf
